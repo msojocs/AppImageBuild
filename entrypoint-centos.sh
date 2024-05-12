202352e28bca0b81da1d0788f110ede6d0c3a214
msojocs <jiyecafe@gmail.com>
2024-05-12 09:39:13 +0800

    fix: centos7-i386

--- entrypoint-centos.sh
+++ entrypoint-centos.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
 set -eo pipefail
-
-source /opt/rh/devtoolset-10/enable
+if [ "$ARCH" == "i386" ];then
+    source /opt/rh/devtoolset-9/enable
+else
+    source /opt/rh/devtoolset-10/enable
+fi
 
 export PATH=/deps/bin/:"$PATH"
 export PKG_CONFIG_PATH=/deps/lib/pkgconfig:"$PKG_CONFIG_PATH"
