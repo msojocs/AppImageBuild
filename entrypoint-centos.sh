1ae0267f3ff44b6df20b42bd04978d9d7142e32a
msojocs <jiyecafe@gmail.com>
2024-05-13 19:48:34 +0800

    perf: use devtoolset-9

--- entrypoint-centos.sh
+++ entrypoint-centos.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
 set -eo pipefail
-if [ "$ARCH" == "i386" ];then
-    source /opt/rh/devtoolset-9/enable
-else
-    source /opt/rh/devtoolset-10/enable
-fi
+source /opt/rh/devtoolset-9/enable
 
 export PATH=/deps/bin/:"$PATH"
 export PKG_CONFIG_PATH=/deps/lib/pkgconfig:"$PKG_CONFIG_PATH"
